fc0e291ef2038508e47b4b7b85cadccc1fe2a8dd
Tuki <tuki@Tukis-MacBook-Pro.local>
2024-04-09 17:20:49 +0800

    refactor: remove useless code

--- src/views/PageDetail.vue
+++ src/views/PageDetail.vue
@@ -2,22 +2,16 @@
   <button class="common-button" @click="backToHome">
     Back to Service Hub
   </button>
-  <ServiceVersions :versions="versions" />
+  <ServiceVersions />
 </template>
 
 <script  lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import ServiceVersions from '@/components/ServiceVersions.vue'
-import type { ServiceVersion } from '@/types/service'
 
 export default defineComponent({
   name: 'PageDetail',
   components: { ServiceVersions },
-  setup() {
-    const versions:ServiceVersion[] = reactive([])
-
-    return { versions }
-  },
   methods: {
     backToHome() {
       this.$router.push('/')
